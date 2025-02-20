7877b39d8a0223f71348199a601ae959821e83b1
SHIN BEOM SU <qjatn147852@naver.com>
2025-02-20 22:48:31 +0900

    250220_DungeonScene popupcanvas <-> DungeonInformation 정보 동기화 로직 제작 완료
    1. 각 던전 정보와 UI를 연결하는 DungeonInfoPrintToPanel.cs 제작.
    2. DungeonScene에서 Trigger이벤트를 통해 PopupCanvas 활성화 시 DungeonInformation 스크립터블 오브젝트의 정보가 UI에 동기화됨.
    3. 각 던전 입구 오브젝트에 DungeonInfoPrintToPanel.cs, DungeonEntranceTrigger.cs를 추가하여 실행.

--- Assets/5. Scripts/DungeonScene/DungeonEntranceTrigger.cs
+++ Assets/5. Scripts/DungeonScene/DungeonEntranceTrigger.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class DungeonEntranceTrigger : MonoBehaviour
 {
     [SerializeField] private Canvas popupCanvas;
+    private DungeonInfoPrintToPanel dungeonInfoPrintToPanel;
+
+    private void Start()
+    {
+        dungeonInfoPrintToPanel = gameObject.GetComponent<DungeonInfoPrintToPanel>();//해당 오브젝트에 부착된 DungeonInfoPrintToPanel 스크립트를 가져온다. 각 오브젝트마다 독립된 스크립터블 오브젝트가 필요하기에, SerializeField로 할당하지 않고 던전 오브젝트 개별적으로 갖고있는 DungeonInfoPrintToPanel 클래스를 GetComponent로 가져온다.
+    }
 
     private void OnEnable()
     {
@@ -18,6 +24,7 @@ public class DungeonEntranceTrigger : MonoBehaviour
         if (popupCanvas != null && gameObject.scene.isLoaded) // 씬이 변경된 경우에도 canvas 참조 오류가 발생하는 것을 확인하여, 이를 방지하기 위해 제한을 추가.
         {
             popupCanvas.gameObject.SetActive(false);
+            dungeonInfoPrintToPanel.ClearDungeonInfo();//팝업 캔버스 비활성화 시 출력되었던 던전 정보 초기화.
         }
     }
 
@@ -29,6 +36,7 @@ public class DungeonEntranceTrigger : MonoBehaviour
             {
                 Time.timeScale = 0;
                 popupCanvas.gameObject.SetActive(true);
+                dungeonInfoPrintToPanel.SynchronizeDungeonInfo();//던전 정보 출력.
             }     
         }
     }
---
+++ Assets/5. Scripts/DungeonScene/DungeonInfoPrintToPanel.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DungeonInfoPrintToPanel : MonoBehaviour
+{
+    // DungeonScene popupcanvas <-> DungeonInformation 정보를 동기화하는 클래스. DungeonScene에서 Trigger이벤트를 통해 PopupCanvas가 활성화되면, Trigger 이벤트가 발생한 던전입구의 정보가 PopupCanvas에 출력된다.
+    //각 던전의 정보는 스크립터블 오브젝트로 관리되며, PopupCanvas에서 전투 시작 버튼을 누르면 해당 던전의 정보와 함께 SinglePlayScene으로 넘어간다.
+    //본 클래스는 각 던전 입구 오브젝트에 추가되고, 인스펙터에서 던전 입구 오브젝트에 부합하는 DungeonInformation 스크립터블 오브젝트를 할당할 수 있다.
+    //본 클래스의 메서드들은 DungeonEntranceTrigger 클래스에서 호출된다.
+    [SerializeField] private TextMeshProUGUI dungeonNameText;
+    [SerializeField] private TextMeshProUGUI rewardAmountText01;
+    [SerializeField] private TextMeshProUGUI rewardAmountText02;
+    [SerializeField] private Image rewardSprite01;
+    [SerializeField] private Image rewardSprite02;
+    [SerializeField] private DungeonInformation dungeonInformation;//던전 정보 스크립터블 오브젝트
+
+    public void SynchronizeDungeonInfo()//던전 정보를 팝업창에 출력하는 메서드.
+    {
+        dungeonNameText.text = dungeonInformation.dungeonName;
+        rewardAmountText01.text = dungeonInformation.firstRewardAmount.ToString();
+        rewardAmountText02.text = dungeonInformation.secondRewardAmount.ToString();
+        rewardSprite01.sprite = dungeonInformation.firstRewardSprite;
+        rewardSprite02.sprite = dungeonInformation.secondRewardSprite;
+    }
+
+    public void ClearDungeonInfo()//던전 정보를 초기화하는 메서드. PopupCanvas가 비활성화될 때(전투 시작 버튼을 누르지 않고 팝업을 끄거나, SinglePlayScene으로 진입하여 씬이 전환될 때 호출.)
+    {
+        dungeonNameText.text = "";
+        rewardAmountText01.text = "";
+        rewardAmountText02.text = "";
+        rewardSprite01.sprite = null;
+        rewardSprite02.sprite = null;
+    }
+}
