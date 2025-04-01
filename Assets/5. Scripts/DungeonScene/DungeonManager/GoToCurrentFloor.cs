9a9d25d5a9bbea5aabb3f272c42b6a7503bdbc5b
SHIN BEOM SU <qjatn147852@naver.com>
2025-04-02 00:16:27 +0900

    250402_각 층의 던전 생성 및 관리 로직 설계
    1. 1층 ~ 50층까지 씬 이동 로직은 설계 완료.
    2. 이제 각 층의 던전 갯수 및 던전의 랜덤 생성 기능을 설계하고,
    3. 던전 클리어 시 던전 클리어 여부를 true로 체크한 후 DungeonScene으로 돌아왔을 때 해당 던전에 접근을 제한하는 기능,
    4. DungeonScene 입장 시 마다 각 층과 던전의 클리어 여부를 체크하여 타일맵에 반영하는 기능
     ==> 각 DungeonScene에서 등장할 던전들은 종류가 정해져 있기 때문에, 각 층 별 던전SO를 리스트에 넣고(총 5개)  -> DungeonScene 입장 시 floor에 맞는 SO 리스트를 가져온다. -> FloorCleared(), DungeonCleared()로 클리어여부 체크 -> 해당없으면 그대로 불러옴.
    
    코드 흐름
    [ 던전 입구 버튼 클릭 ] -> [ GoToCurrentFloor.cs에서 CheckCurrentFloor()실행 ] -> [ DungeonScene 입장 ]
    -> [ 저장되어있던 던전SO 리스트 5개 중  currentFloor / 10에 해당하는 리스트를 찾는다. ] -> [ 그 리스트에 존재하는 던전 정보를 DungeonScene에 위치한 던전 스폰 트랜스폼에 랜덤으로 배치
    
    5. DungeonScene Layer 구성 변경 및 컬라이더 요소 재배치
    - Layer01, 02, 03으로 구분.
    - Layer02의 컬라이더가 어긋나있는 오류 발견 -> 다시 제작함.

--- Assets/5. Scripts/DungeonScene/DungeonManager/GoToCurrentFloor.cs
+++ Assets/5. Scripts/DungeonScene/DungeonManager/GoToCurrentFloor.cs
@@ -2,38 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GoToCurrentFloor : MonoBehaviour//현재 플레이어의 층 수 정보를 받아와, 해당하는 층의 Scene으로 이동하는 스크립트.
+public class GoToCurrentFloor : MonoBehaviour//현재 플레이어의 층 수 정보를 받아와, 해당하는 층의 Scene으로 이동하는 스크립트. MainScene의 던전입구 버튼에 붙여서 쓰이고, SinglePlayScene에서 "다음 던전" 또는 "던전 입구"로 돌아갈 때에도 사용.
 {
-    private string[] floors = {"1_9Floor", "10_19Floor", "20_29Floor", "30_39Floor", "40_49Floor", "50Floor"};//각 층의 던전 이름을 저장하는 배열. 1층부터 50층까지의 던전 이름을 저장.
-    
+    private string[] floors = {"DungeonScene", "10_19Floor", "20_29Floor", "30_39Floor", "40_49Floor", "50Floor"};//각 층의 던전 이름을 저장하는 배열. 1층부터 50층까지의 던전 이름을 저장.
+    [SerializeField] private Button button;
+    void Start()
+    {
+        button.onClick.AddListener(CheckCurrentFloor);//버튼 클릭 시 CheckCurrentFloor 메서드 호출.
+    }
+
     private void CheckCurrentFloor()//플레이어의 현재 층 수를 체크하고, 해당하는 씬으로 이동하는 메서드.
     {
         int currentFloor = PlayerInfo.Instance.GetPlayerFloor();//플레이어의 현재 층 수를 가져옴.
         switch(currentFloor / 10)//플레이어의 층 수에 따라 이동할 씬을 결정.
         {
             case 0:
-                LoadCurrentFloorScene(1);//1층 -> 10_19Floor 씬으로 이동.
+                LoadCurrentFloorScene(0);//1_9Floor 씬으로 이동.
                 break;
 
             case 1:
-                LoadCurrentFloorScene(1);//2층 -> 20_29Floor 씬으로 이동.
+                LoadCurrentFloorScene(1);//10_19Floor 씬으로 이동.
                 break;
 
             case 2:
-                LoadCurrentFloorScene(2);//3층 -> 30_39Floor 씬으로 이동.
+                LoadCurrentFloorScene(2);//20_29Floor 씬으로 이동.
                 break;
 
             case 3:
-                LoadCurrentFloorScene(3);//4층 -> 40_49Floor 씬으로 이동.
+                LoadCurrentFloorScene(3);//30_39Floor 씬으로 이동.
                 break;
 
             case 4:
-                LoadCurrentFloorScene(4);//5층 -> 50Floor 씬으로 이동.
+                LoadCurrentFloorScene(4);//40_49Floor 씬으로 이동.
                 break;
 
             case 5 :
-                LoadCurrentFloorScene(5);//6층 -> 50Floor 씬으로 이동.
+                LoadCurrentFloorScene(5);//50Floor 씬으로 이동.
                 break;
 
             default:
