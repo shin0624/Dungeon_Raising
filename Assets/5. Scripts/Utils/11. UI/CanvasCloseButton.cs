ceba0958877a6d342beebb761cf80c0dba63b834
SHIN BEOM SU <qjatn147852@naver.com>
2025-04-04 00:54:57 +0900

    250404_DungeonScene 및 1~49층 모든 씬 UI를 DOTWeen으로 변경 완료.
    1. UI 컨트롤러 클래스에서 DOTWeen 기능을 만들지 않고 바로 사용할 수 있게 하는 DOTWeenUIAnimation.,cs 사용.
    2. 던전 씬은 좀 다르게 사용되어야 하는데, 던전정보 캔버스가 호출되면 던전 정보 동기화 + TimeScale값을 다루어야 하기 때문에 코루틴으로 프레임 단위 타이밍을 조절.
    3. 던전 정보 캔버스는 closeButton도 따로 canvasCloseButton.cs가 존재하므로, SerializeField에서 targetPanel, DungeonEnterPanel 할당 필요.

--- Assets/5. Scripts/Utils/11. UI/CanvasCloseButton.cs
+++ Assets/5. Scripts/Utils/11. UI/CanvasCloseButton.cs
@@ -8,6 +8,7 @@ public class CanvasCloseButton : MonoBehaviour
 {
     [SerializeField] private Button closeButton;
     [SerializeField] private Canvas popupCanvas;
+    [SerializeField] private GameObject targetPanel;
     void Start()
     {
         closeButton.onClick.AddListener(OnCloseButtonClicked);
@@ -21,6 +22,7 @@ public class CanvasCloseButton : MonoBehaviour
             if(Time.timeScale==0)
             {
                 Time.timeScale = 1;
+                DOTWeenUIAnimation.PopupDownAnimationInUI(targetPanel, Vector3.zero, 0.2f);
             }
         }
         else
