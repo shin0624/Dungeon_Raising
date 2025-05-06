9ef128dba0bd6cf86d9ff1d4b55771025bd9e752
SHIN BEOM SU <qjatn147852@naver.com>
2025-05-06 16:59:22 +0900

    250506_강화 및 승급 시스템 작성
    1. 장비(Armor) 레벨업 및 승급 시스템 설계
    	레벨업에 필요한 재화, 골드 연결
    	레벨업, 승급 시 아이템 스프라이트 변화
    	실제 데이터에 반영
    
    - 현재 레벨업, 승급 ui는 모두 제작되어 있고, 버튼 기능 할당도 끝난 상태
    - 장비 레벨업을 누르면 BlackSmithUIController.cs의 PerformCharacterLevelUp() 호출
    - 장비 승급을 누르면 BlackSmithUIController.cs의 PerformCharacterUpgrade() 호출. -> 위 두 메서드에 실제 강화승급 로직을 작성해야 함.
    
    2. 장비 레벨업 (강화) 시스템 아키텍처 설계
    	A. 레벨업 버튼 클릭 시
    		- 재화 보유량 체크 ->[ 보유량 > 필요량 ] -> 레벨업 수행, 재화 소모, 레벨 ui 변경
    				          -> [ 보유량 < 필요량 ] -> 레벨업 수행 불가, 재화 부족 팝업 호출
    	B. 레벨업 로직
    		- 공격력 + N
    		- 레벨 + 1
    		- 재화 - N
    		- 각 등급 별 레벨 한도 달성 시 승급 가능. -> 승급 시 등급 상승 및 스프라이트 이미지 변경
    		- 레벨업 1회 마다 정보 저장
    
    	C. 레벨업 모듈 제작
    		- PlayerArmorLevelup.cs
    		- 현재 사용되는 클래스
    			- BlackSmithPlayerImageSetting
    			- BlackSmithUIController
    			- BlackSmithPlayerItemInfo
    			- BlackSmithItemList
    			- BlackSmithItemElementClicker
    
    3. 장비 레벨업 - 장비 승급 패널 분리 필요
    	- 현재 강화대상 아이템 ui 위에 레벨업/승급버튼이 있어서, 각 버튼 역할에 맞는 ui 위치 배정이 필요.
    	- 레벨업 클릭 시 [현재 레벨 -> 다음 레벨]을 표시하고, 승급 클릭 시 [현재 등급 -> 다음 등급]으로 표시되어야 함.
    	-> LevelUpPanel / GradeUpPanel 두 패널로 나누었음.
    	- 오류 : 패널을 두개로 나눈 후 아이템 리스트에서 엘리먼트 클릭 시 null오류
    		- 원인 : 인스펙터에서 아이템 별 GradeUpCostAmout텍스트 재할당 필요
    		- 해결방법 : 인스펙터에서 아이템 별 GradeUpCostAmout텍스트 재할당
    
    	- 승급 버튼 클릭 시 아이템 스프라이트, 아이템등급(before, after) 각각 표시되도록 수정
    	- ElementLink() 메서드를 수정하였음.-> LevelUpPanel과 GradeUpPanel의 active 여부에 따라 각각 패널이 교체되도록 하였고, GradeUpPanel 활성화 시 armorItem.itemGrade의 enum타입 특성을 활용하여 text에 표시.
    	- 인스펙터에서 LevelUp, GradeUp 역할 별 Before, After 이미지 각각 할당하여 스프라이트 표시.
    	- ItemGrade에서, BlackStone 이후는 itemGrade+1 = 5로 표시되기 때문에,  "최고등급"으로 바꾸어 표기하도록 수정.

--- Assets/5. Scripts/MainScene/BlackSmith/BlackSmithItemElementClicker.cs
+++ Assets/5. Scripts/MainScene/BlackSmith/BlackSmithItemElementClicker.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BlackSmithItemElementClicker : MonoBehaviour
 {
-    [SerializeField] private Image beforeImage;//레벨업 버튼 아래에 표시될 아이템 이미지
-    [SerializeField] private Image afterImage;//승급 버튼 아래에 표시될 아이템 이미지
+    [SerializeField] private Image beforeLevelUpImage;//레벨업 아이템 이미지
+    [SerializeField] private Image afterLevelUpImage;//레벨업 아이템 이미지
+    [SerializeField] private Image beforeGradeImage;//승급 아이템 이미지
+    [SerializeField] private Image afterGradeImage;//승급  아이템 이미지
     [SerializeField] private TextMeshProUGUI beforeLevel;// [ 현재레벨(itemLevel) / 최대레벨(10)]
     [SerializeField] private TextMeshProUGUI beforeStatus;// 무기일 경우 [공격력(offensivePower) n], 방어구일 경우 [방어력(defensivePower) n]
     [SerializeField] private TextMeshProUGUI afterLevel;// [ 현재레벨(itemLevel) + 1 / 최대레벨(10)]
     [SerializeField] private TextMeshProUGUI afterStatus; //무기일 경우 [공격력(offensivePower) + 상승치 n], 방어구일 경우 [방어력(defensivePower) = 상승치 n]
     [SerializeField] private TextMeshProUGUI levelUpCost;
     [SerializeField] private TextMeshProUGUI gradeUpCost;
+    [SerializeField] private GameObject[] reinforcementPanels = new GameObject[2];// 레벨업 패널과 승급 패널을 구분하기 위한 배열. [0] : 레벨업 패널, [1] : 승급 패널
+    [SerializeField] private TextMeshProUGUI beforeGrade;
+    [SerializeField] private TextMeshProUGUI afterGrade; 
+    
 
     // 레벨 및 스테이터스 상승치 등은 추후 수치 정립이 완료되면 추가. 
 
@@ -22,34 +27,43 @@ public class BlackSmithItemElementClicker : MonoBehaviour
     {
         Button itemElementbutton = newItem.GetComponent<Button>();
         itemElementbutton.onClick.AddListener(() => ElementLink(newItem, armorItem));
-
-        // Debug.Log("리스트 각 버튼에 리스너 등록됨.");
-        // Debug.Log($"리스너가 등록된 엘리먼트 : {newItem.name}");
-        // Debug.Log($"리스너가 등록된 장비 아이템 : {armorItem.itemName}");
     }
 
     private void ElementLink(GameObject newItem, ArmorItem armorItem)//BlackSmithItemList.cs의 PopulateItemList()에서 리스트에 출력된 아이템 버튼 클릭 시 해당 아이템 정보를 RightPivot에 표시하는 메서드
     {   
-       //Debug.Log($"{armorItem.itemParts} 부위 아이템 리스트에서 {armorItem.itemName}클릭함. ");
+       //250506 수정 : RightPivot을 LevelUpPanel과 GradeUpPanel로 나누어, 각각의 패널에 맞는 정보를 표시하도록 수정.
 
-       beforeImage.sprite = newItem.transform.Find("ArmorIcon").GetComponent<Image>().sprite;
-       afterImage.sprite = newItem.transform.Find("ArmorIcon").GetComponent<Image>().sprite;
-
-       beforeLevel.text = $"Lv {armorItem.itemLevel} / 10";
-       afterLevel.text = $"Lv {armorItem.itemLevel + 1} / 10";
-
-       levelUpCost.text = $"{PlayerInfo.Instance.GetplayerGold()} / {armorItem.levelUpCost}";
-       gradeUpCost.text = $"{PlayerInfo.Instance.GetplayerGold()} / {armorItem.gradeUpCost}";
-
-        if(armorItem.itemParts == ItemPart.Weapon) 
+        if(reinforcementPanels[0].activeSelf)//레벨업 패널이 활성화 되어있다면
         {
-            beforeStatus.text = $"공격력 {armorItem.offensivePower}";
-            afterStatus.text = $"공격력 {armorItem.offensivePower + 10}";//상승치는 일단 임시로 설정.
+            beforeLevelUpImage.sprite = newItem.transform.Find("ArmorIcon").GetComponent<Image>().sprite;
+            afterLevelUpImage.sprite = newItem.transform.Find("ArmorIcon").GetComponent<Image>().sprite;
+            beforeLevel.text = $"Lv {armorItem.itemLevel} / 10";
+            afterLevel.text = $"Lv {armorItem.itemLevel + 1} / 10";
+            levelUpCost.text = $"{PlayerInfo.Instance.GetplayerGold()} / {armorItem.levelUpCost}";
+            switch(armorItem.itemParts)
+            {
+                case ItemPart.Weapon:
+                    beforeStatus.text = $"공격력 {armorItem.offensivePower}";
+                    afterStatus.text = $"공격력 {armorItem.offensivePower + 10}";//상승치는 일단 임시로 설정.
+                    break;
+                default:
+                    beforeStatus.text = $"방어력 {armorItem.defensivePower}";
+                    afterStatus.text = $"방어력 {armorItem.defensivePower + 10}";//상승치는 일단 임시로 설정.
+                    break;
+            }
         }
-        else
+        else if(reinforcementPanels[1].activeSelf)//승급 패널이 활성화 되어있다면
         {
-            beforeStatus.text = $"방어력 {armorItem.defensivePower}";
-            afterStatus.text = $"방어력 {armorItem.defensivePower + 10}";//상승치는 일단 임시로 설정.
+            beforeGradeImage.sprite = newItem.transform.Find("ArmorIcon").GetComponent<Image>().sprite;
+            afterGradeImage.sprite = newItem.transform.Find("ArmorIcon").GetComponent<Image>().sprite;
+            gradeUpCost.text = $"{PlayerInfo.Instance.GetplayerGold()} / {armorItem.gradeUpCost}";
+            beforeGrade.text = armorItem.itemGrade.ToString();
+            afterGrade.text = ((Grade)((int)armorItem.itemGrade + 1)).ToString();//enum은 내부적으로 int값으로 동작하므로, itemGrade + 1이 허용된다. 하지만, ToString()으로 text출력을 위해서는 타입 캐스트가 필요.
+            if(afterGrade.text == "5")//BlackStone 등급은 승급이 불가능.
+            {
+                afterGrade.text = "BlackStone";
+                gradeUpCost.text = "승급 불가";
+            }
         }
     }
 
